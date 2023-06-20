3f2471dcd5c46a370aec3e838ef0fea582e7d2e1
babotavo419 <one.faith419@gmail.com>
2023-06-20 05:59:34 -0400

    product context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
-
+import { ProductContext } from './contexts/ProductContext';
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
@@ -13,22 +13,26 @@ function App() {
 
 	const addItem = item => {
 		// add the given item to the cart
+		setCart([...cart, item]);
 	};
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}> {/* use the provider */}
+			<div className="App">
+				<Navigation cart={cart} />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+				{/* Routes */}
+				<Route exact path="/">
+					<Products />
+				</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
-		</div>
+				<Route path="/cart">
+					<ShoppingCart cart={cart} />
+				</Route>
+			</div>
+		</ProductContext.Provider> // close the provider
 	);
 }
 
 export default App;
+
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,22 @@
-import React from 'react';
-
-// Components
+import React, { useContext } from 'react';
+import { ProductContext } from '../contexts/ProductContext'; // import the ProductContext
 import Product from './Product';
 
-const Products = props => {
-	return (
-		<div className="products-container">
-			{props.products.map(product => (
-				<Product
-					key={product.id}
-					product={product}
-					addItem={props.addItem}
-				/>
-			))}
-		</div>
-	);
+const Products = () => {
+  const { products, addItem } = useContext(ProductContext); // consume the ProductContext
+
+  return (
+    <div className="products-container">
+      {products.map(product => (
+        <Product
+          key={product.id}
+          product={product}
+          addItem={addItem}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Products;
+
