c8a88de512bf675a81ed8f5ee0911e3484335d6b
babotavo419 <one.faith419@gmail.com>
2023-06-20 06:42:27 -0400

    cart persist

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 import { ProductContext } from './contexts/ProductContext';
@@ -10,51 +10,56 @@ import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
 function App() {
-    const [products] = useState(data);
-    const [cart, setCart] = useState([]);
+  const [products] = useState(data);
+  const [cart, setCart] = useState(() => {
+    const storedCart = localStorage.getItem('cart');
+    return storedCart ? JSON.parse(storedCart) : [];
+  });
 
-	const addItem = item => {
-		const existingItem = cart.find(cartItem => cartItem.id === item.id);
-	
-		if (existingItem) {
-			const updatedCart = cart.map(cartItem => {
-				if (cartItem.id === item.id) {
-					return { ...cartItem, quantity: cartItem.quantity + 1 };
-				}
-				return cartItem;
-			});
-	
-			setCart(updatedCart);
-		} else {
-			setCart([...cart, { ...item, quantity: 1 }]);
-		}
-	};
-	
+  const addItem = item => {
+    setCart(prevCart => {
+      const existingItem = prevCart.find(cartItem => cartItem.id === item.id);
+      if (existingItem) {
+        return prevCart.map(cartItem =>
+          cartItem.id === item.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        );
+      } else {
+        return [...prevCart, { ...item, quantity: 1 }];
+      }
+    });
+  };
 
-    const removeItem = itemId => {
-        setCart(cart.filter(item => item.id !== itemId));
-    };
-
-    return (
-        <ProductContext.Provider value={{ products, addItem }}>
-            <CartContext.Provider value={{ cart, removeItem }}>
-                <div className="App">
-                    <Navigation />
-                    {/* Routes */}
-                    <Route exact path="/">
-                        <Products />
-                    </Route>
-
-                    <Route path="/cart">
-                        <ShoppingCart />
-                    </Route>
-                </div>
-            </CartContext.Provider>
-        </ProductContext.Provider>
+  const removeItem = itemId => {
+    setCart(prevCart =>
+      prevCart.filter(cartItem => cartItem.id !== itemId)
     );
+  };
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
+
+  return (
+    <ProductContext.Provider value={{ products, addItem }}>
+      <CartContext.Provider value={{ cart, removeItem }}>
+        <div className="App">
+          <Navigation />
+          <Route exact path="/">
+            <Products />
+          </Route>
+          <Route path="/cart">
+            <ShoppingCart />
+          </Route>
+        </div>
+      </CartContext.Provider>
+    </ProductContext.Provider>
+  );
 }
 
 export default App;
+
 
 
 
